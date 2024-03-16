aa2d7f40b826f9eb3934f0677379b2213617a0b9
mshr0969 <mshr60531@gmail.com>
2024-03-16 13:13:28 +0900

    add update not_found err

--- gateway/internal/repository/paper.go
+++ gateway/internal/repository/paper.go
@@ -121,6 +121,14 @@ func (pr *PaperRepository) UpdatePaper(ctx context.Context, do domain.Paper) err
 		}
 	}()
 
+	err = tx.GetContext(ctx, &do, "SELECT id, title, published, url FROM papers WHERE id=?", do.Id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return domain.ErrNonExistentPaper
+		}
+		return err
+	}
+
 	query, args := createUpdateQuery(do)
 	_, err = tx.ExecContext(ctx, query, args...)
 	if err != nil {
